262dbc8c6b06835f9534453591580c3ecc120f10
efano <efa235@uky.edu>
2021-07-07 14:12:52 -0700

    add constructor

--- script.js
+++ script.js
@@ -14,68 +14,90 @@ const vendorType = document.querySelector('.form__vendor--type');
 const foodType = document.querySelector('.form__food--type');
 const seating = document.querySelector('.form__checkbox--seating');
 
-let map, mapEvent;
+class App {
+  #map;
+  #mapEvent;
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 19
-      });
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      const coords = [latitude, longitude];
-      const options = {
-        center: coords,
-        layers: basemap,
-        zoom: 16
-      };
-      map = L.map('map', options).locate({
-        watch:true
-      });
+  constructor() {
+    this._getPosition();
+    document.getElementById('form__btn').addEventListener('click', this._newLocation.bind(this));
+  }
 
-      L.circleMarker(coords, {
-        interactive: false,
-        className: 'pulse',
-        radius: 3,
-        weight: 2,
-        color: '#F7B40A',
-        fillColor: '#F7B40A',
-        fillOpacity: 0.4
-      }).addTo(map)
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position')
+        }
+      );
+  }
 
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        tabForm.classList.remove('tab-disabled');
-        tabInfo.classList.remove('is-active');
-        tabList.classList.remove('is-active');
-        tabForm.classList.add('is-active');
-        tabContentInfo.classList.remove('is-active');
-        tabContentList.classList.remove('is-active');
-        tabContentForm.classList.add('is-active');
-        vendorType.focus();
-      });
+  _loadMap(position) {
+    const basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 19
+    });
+    const {
+      latitude
+    } = position.coords;
+    const {
+      longitude
+    } = position.coords;
+    const coords = [latitude, longitude];
+    const options = {
+      center: coords,
+      layers: basemap,
+      zoom: 16
+    };
+    this.#map = L.map('map', options).locate({
+      watch: true
+    });
 
-    },
-    function() {
-      alert('Could not get your position')
-    }
-  );
+    L.circleMarker(coords, {
+      interactive: false,
+      className: 'pulse',
+      radius: 3,
+      weight: 2,
+      color: '#F7B40A',
+      fillColor: '#F7B40A',
+      fillOpacity: 0.4
+    }).addTo(this.#map)
 
-  document.getElementById('form__btn').addEventListener("click", function() {
-    //e.preventDefault();
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+      tabForm.classList.remove('tab-disabled');
+      tabInfo.classList.remove('is-active');
+      tabList.classList.remove('is-active');
+      tabForm.classList.add('is-active');
+      tabContentInfo.classList.remove('is-active');
+      tabContentList.classList.remove('is-active');
+      tabContentForm.classList.add('is-active');
+      vendorType.focus();
+  }
+
+  _newLocation(e) {
+    e.preventDefault();
     document.getElementById('form').reset();
 
-    const {lat, lng} = mapEvent.latlng;
+    const {
+      lat,
+      lng
+    } = this.#mapEvent.latlng;
 
-        L.marker([lat,lng])
-          .addTo(map)
-          .bindTooltip(
-            L.tooltip({
-              className: 'stand-tooltip'
-            })
-          )
-          .setTooltipContent('Vegetarian/Vegan Cuisine')
-  });
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindTooltip(
+        L.tooltip({
+          className: 'stand-tooltip'
+        })
+      )
+      .setTooltipContent('Vegetarian/Vegan Cuisine')
+  }
+}
+
+const app = new App();
